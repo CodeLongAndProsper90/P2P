bcfd46672d38cc9628f725185d2f57cdcea0f7ab
Scott Little <k1206150@students.katyisd.org>
2020-01-16 18:01:24 -0600

    rework

--- client.py
+++ client.py
@@ -1,84 +1,29 @@
-# client2.py
-#!/usr/bin/env python
-
 import socket
-import sys
-import time
-import threading
-from hurry.filesize import size as Size
+import tqdm
 import os
-import ast
-TCP_IP = '192.168.1.20'
-TCP_PORT = 9001
-BUFFER_SIZE = 1024
-data = 0
-if len(sys.argv) < 2:
-  print("Missing operand")
-  exit()
-filename = sys.argv[1]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-print("Connected!")
-s.send(f"FILE:{filename}".encode('utf-8'))
-print("Reqesting file...")
+SEP = "<SEP>"
+BUFFER_SIZE = 4096
+host = '192.168.1.20'
+port = 9001
+filename = 'test'
+filesize = os.path.getsize(filename)
 
-if len(sys.argv) < 3:
-  saveas = sys.argv[1]
-else:
-  saveas = sys.argv[2]
-exists = s.recv(BUFFER_SIZE).decode('utf-8')
-if exists.startswith("STATUS:"):
-    exists = exists.replace("STATUS:","",1)
-    print(exists)
-    if exists.startswith('NFile'):
-      print(f'{sys.argv[1]}: No such file or directory')
-      exit(1)
-    else:
-      print(f"File {filename} exists...")
-size = s.recv(BUFFER_SIZE).decode('utf-8')
-print(size.split('#0'))
-if size.startswith("SIZE:"):
-    size = size.replace("SIZE:","",1)
-    size = size.split('#0')
-    data = size
-    size = size[0]
-print(f"Size of file is {Size(int(size))}")
-byte = 0
-print("Processing data...")
-for item in data:
-    print("Item is :" + item)
-    if item.startswith("DATA:"):
-        write = item.replace('DATA:','',1)
-        print("New version: " + write)
-#        write = write.decode('utf-8')
-        print(write)
-        write = ast.literal_eval(write)
-        break
-try:
-  with open(saveas, 'wb') as f:
-      while True:
-          if type(write) is not bytes:
-            write = s.recv(BUFFER_SIZE)
-            print(write)
-          if not write:
-              f.close()
-              break
-          # write data to a file
-          f.write(write)
-          byte+=BUFFER_SIZE
-          if byte < int(size):
-              f.close()
-              break
-          
-          input()
-          print(f"Downloaded {Size(byte)} of {Size(int(size))}",end='\r')
-          write = None 
-except KeyboardInterrupt or EOFError:
-#  os.remove(filename)
-    print()
-print('Successfully got the file')
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected")
+
+s.send(f'{filename}{SEP}{filesize}'.encode())
+
+progress = tqdm.tqdm(range(filesize), f'Sending {filename}', unit='B', unit_scale=True, unit_divisor=1024)
+
+with open(filename, "rb") as f:
+  for _ in progress:
+    bytes_read = f.read(BUFFER_SIZE)
+    if not bytes_read:
+      break
+    s.sendall(bytes_read)
+    progress.update(len(bytes_read))
 s.close()
-print('connection closed')
-with open(saveas, 'r') as f:
-    print(f.read())
+
--- server.py
+++ server.py
@@ -1,71 +1,34 @@
-# server1.py
 import socket
-from threading import Thread
-from socketserver import ThreadingMixIn
+import tqdm 
 import os
 
-TCP_IP = '192.168.1.85'
-TCP_PORT = 9001
-BUFFER_SIZE = 1024
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 9001
+BUFFER_SIZE = 4096
+SEP = "<SEP>"
+saveas = 'test.img.trans'
+s = socket.socket()
 
-class ClientThread(Thread):
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
 
-    def __init__(self,ip,port,sock):
-        Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.sock = sock
-        print(" New thread started for "+ip+":"+str(port))
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+client_socket, address = s.accept()
 
-    def size(self, path):
-      return os.path.getsize(path)
-    def run(self):
-        filename=self.sock.recv(1024)
-        filename = filename.decode("utf-8") 
-        
-        if filename.startswith("FILE:"):
-          filename = filename.replace("FILE:",'',1)
-        if not os.path.exists(filename):
-          conn.send('STATUS:NFile#0'.encode('utf-8')) # Send the file status
-          conn.close()
-          return
-        else:
-          conn.send("STATUS:File#0".encode('utf-8'))
-        print(f"Size of {filename} is {self.size(filename)}") 
-        filesize = self.size(filename)
-        conn.send(f"SIZE:{str(filesize)}#0".encode('utf-8')) # Send filesize
-        f = open(filename, 'a')
-        f.write("#e")
-        f.close()
-        f = open(filename,'rb')
-        l = f"DATA:{f.read(BUFFER_SIZE-5)}"
-        
-        while True:
-          while (l):
-              self.sock.send(l.encode('utf-8'))
-              l = f.read(BUFFER_SIZE)
-          if not l:
-              self.sock.send('#e'.encode('utf-8'))
-              f.close()
-              self.sock.close()
-              break
-        with open(filename, 'r+') as f:
-          source = f.read().strip().split('\n')
-          source[-1] = source[-1].replace('#e','',-1)
 
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-threads = []
+received = client_socket.recv(BUFFER_SIZE).decode()
+print(received)
+filename, filesize = received.split(SEP)
+filename = os.path.basename(filename)
+filesize = int(filesize)
 
-while True:
-    tcpsock.listen(5)
-    print("Waiting for incoming connections...")
-    (conn, (ip,port)) = tcpsock.accept()
-    print('Got connection from ', (ip,port))
-    newthread = ClientThread(ip,port,conn)
-    newthread.start()
-    threads.append(newthread)
-
-for t in threads:
-    t.join()
+progress = tqdm.tqdm(range(filesize), f"Reciving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(saveas, "wb") as f:
+  for _ in progress:
+    bytes_read = client_socket.recv(BUFFER_SIZE)
+    if not bytes_read:
+      break
+  f.write(bytes_read)
+  progress.update(len(bytes_read))
+client_socket.close()
+s.close()
