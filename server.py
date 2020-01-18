d4c21a3d9f2ab97cb8a28149a38856a221d3452f
Scott <none@domain.com>
2020-01-18 13:33:24 -0600

    It works!

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 9001
 BUFFER_SIZE = 4096
 SEP = "<SEP>"
-saveas = 'test.img.trans'
 s = socket.socket()
 
 s.bind((SERVER_HOST, SERVER_PORT))
@@ -20,15 +19,17 @@ received = client_socket.recv(BUFFER_SIZE).decode()
 print(received)
 filename, filesize = received.split(SEP)
 filename = os.path.basename(filename)
+saveas = filename
 filesize = int(filesize)
 
 progress = tqdm.tqdm(range(filesize), f"Reciving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(saveas, "wb") as f:
   for _ in progress:
     bytes_read = client_socket.recv(BUFFER_SIZE)
+    print(bytes_read)
     if not bytes_read:
       break
-  f.write(bytes_read)
-  progress.update(len(bytes_read))
+    f.write(bytes_read)
+    progress.update(len(bytes_read))
 client_socket.close()
 s.close()
